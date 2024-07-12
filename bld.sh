0a750f922204b158b427a6a2aac967400c31beaa
Steve Ford <sford@geeky-boy.com>
2024-07-12 12:57:47 -0400

    bugfix

--- bld.sh
+++ bld.sh
@@ -7,8 +7,7 @@ for F in *.md; do :
     # Update doc table of contents (see https://github.com/fordsfords/mdtoc).
     if which mdtoc.pl >/dev/null; then mdtoc.pl -b "" $F;
     elif [ -x ../mdtoc/mdtoc.pl ]; then ../mdtoc/mdtoc.pl -b "" $F;
-    else echo "FYI: mdtoc.pl not found; see https://github.com/fordsfords/mdtoc"; exit 1
-    fi
+    else echo "FYI: mdtoc.pl not found; Skipping doc build"; echo ""; fi
   fi
 done
 
@@ -21,9 +20,13 @@ fi
 
 rm -rf *.class *.jar
 
+echo "Building code"
+
 # Build json print module.
 javac -cp ./LBMMonCallbacks.class:$L/MCS/lib/MCS.jar:$L/MCS/lib/UMS_6.15.jar:$L/MCS/lib/UMSMON_PROTO3.jar:$L/MCS/lib/um-mondb-common.jar:$L/MCS/lib/protobuf-java-util-4.0.0-rc-2.jar:$L/MCS/lib/protobuf-java-4.0.0-rc-2.jar:$L/MCS/lib/gson-2.8.5.jar:$L/MCS/lib/java-getopt-1.0.13.jar:$L/MCS/lib/log4j-api-2.14.1.jar:$L/MCS/lib/log4j-core-2.14.1.jar:$L/MCS/lib/guava-24.1.1-jre.jar JsonPrint.java
 if [ "$?" -ne 0 ]; then echo "`date` Error" >&2; exit 1; fi
 
 jar cf JsonPrint.jar *.class
 if [ "$?" -ne 0 ]; then echo "`date` Error" >&2; exit 1; fi
+
+echo "Success"
